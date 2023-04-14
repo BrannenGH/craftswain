e91276bcd15ff16275501360850f1cdcfb8d35f2
Brannen Hall <Brannen@HallB.me>
2023-04-14 01:45:08 +0000

    Update settings for e2e tests

--- packages/jest/index.ts
+++ packages/jest/index.ts
@@ -1,3 +1,12 @@
 import { CraftswainEnvironment } from "./craftswain-environment";
 
+declare const global: any;
+
+/**
+ * Retrieve a test object from the store.
+ * @param name The test object to get.
+ * @returns The test object.
+ */
+export const get = global.get as <T>(name: string) => PromiseLike<T>; 
+
 export default CraftswainEnvironment;
---
+++ packages/playwright/e2e/craftswain.config.ts
@@ -0,0 +1,18 @@
+import type { CraftswainConfig } from "@craftswain/core";
+
+const config: CraftswainConfig = {
+  rootDirectory: ".",
+  testObjects: [
+    {
+        name: "playwrightDocker",
+        type: "@craftswain/docker",
+        image: "mcr.microsoft.com/playwright:v1.12.3-focal",
+    },
+    {
+        name: "localPlaywright",
+        type: "@craftswain/playwright",
+    }
+  ],
+};
+
+export default config;
